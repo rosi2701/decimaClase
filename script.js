fd0bd8ca2b29cb1fbd65028fba27bf2f13406c24
rosi2701 <rcsfuentes@gmail.com>
2025-10-20 23:22:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const portfolio = document.querySelector("#porotito");
+
+async function datos(raw) {
+    try {
+        let consulta = await fetch(raw);
+        let trabajos = await consulta.json();
+        console.log(trabajos);
+        trabajos.forEach((trabajo) => {
+            portfolio.innerHTML += `
+
+                            <div class="col">
+                                <div class="card shadow-sm">
+                                <img src="${trabajo.photo}" class="card-img-top">
+                                <div class="card-body">
+                                <p class="card-text">${trabajo.title}</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-outline-secondary">${trabajo.category}</button>
+                                </div>
+                                <small class="text-body-secondary">Reciente </small>
+             </div>
+             </div>
+            </div>
+            </div>`;
+        });
+    } catch (error) {
+        console.error("Error al cargar los datos:", error);
+        console.error("Detalles:", error);
+    }
+}
+
+//datos("")
